03300877acb0f57ee7d67135917fc65024041219
Sai Menda <saimenda@Sureshs-MacBook-Pro.local>
2015-02-08 22:35:23 -0800

    add

---
+++ nagios-master/README.md
@@ -0,0 +1,1 @@
+### Run the following URL to install nagios.
---
+++ nagios-master/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+## Source Common Functions
+curl -s "https://raw.githubusercontent.com/linuxautomations/scripts/master/common-functions.sh" >/tmp/common-functions.sh
+#source /root/scripts/common-functions.sh
+source /tmp/common-functions.sh
+
+## Checking Root User or not.
+CheckRoot
+
+## Checking SELINUX Enabled or not.
+CheckSELinux
+
+## Checking Firewall on the Server.
+CheckFirewall
+
+curl -s https://assets.nagios.com/downloads/nagiosxi/install.sh | bash
+
+
+
